19b01f1a43c85cf491d2fba785c69721d97ae9a2
Zegendary <zxw1992513@qq.com>
2019-02-27 11:31:20 +0800

    使用 相对 协议

--- src/config/api-prefix.ts
+++ src/config/api-prefix.ts
@@ -4,7 +4,7 @@ let apiPrefix:string = '';
 if(ENV === 'development'){
 	apiPrefix = '//localhost:3000';
 }else if(ENV === 'production'){
-	apiPrefix = 'http://gp-server.hunger-valley.com';
+	apiPrefix = '//gp-server.hunger-valley.com';
 }
 
 export default apiPrefix
