fd13e8a029c7b6c30722af6f230bf684b8f6189a
Zegendary <zxw1992513@qq.com>
2019-03-14 16:42:12 +0800

    改为 hashRouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { HashRouter as Router, Route, Switch } from 'react-router-dom';
 import Index from './components/Index/Index'
 import Login from './components/Login/Login'
 import SignUp from './components/SignUp/SignUp'
